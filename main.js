351f64aa6ba64c245884434f9fcccaf7b77c9e90
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-02-21 00:23:14 -1000

    Add Tree class and method to sort and remove duplicates

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ class Node {
     this.left = null;
   }
 }
+
+class Tree {
+  constructor(array = []) {
+    this.tree = this.arraySortRemove(array);
+  }
+
+  // arraySortRemove(array) sorts and removes duplicates in an array;
+  arraySortRemove(array) {
+    let sorted = array.sort((a, b) => a - b);
+    return sorted.filter((item, index) => sorted.indexOf(item) === index);
+  }
+}
