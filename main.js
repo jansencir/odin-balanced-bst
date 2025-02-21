c9ce3b75e42d7aa97bae0267b433b44629c8a176
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-02-21 00:59:44 -1000

    Add method to build tree to Tree class
    Also rework arraySortRemove method, and constructor to modify array during initialization

--- main.js
+++ main.js
@@ -2,19 +2,40 @@
 class Node {
   constructor(data) {
     this.root = data;
-    this.right = null;
     this.left = null;
+    this.right = null;
   }
 }
 
 class Tree {
-  constructor(array = []) {
-    this.tree = this.arraySortRemove(array);
+  constructor(arr = []) {
+    // Sort and remove duplicates in the original array;
+    this.array = this.arraySortRemove(arr);
+    this.tree = this.buildTree(this.array, 0, this.array.length - 1);
   }
 
   // arraySortRemove(array) sorts and removes duplicates in an array;
   arraySortRemove(array) {
-    let sorted = array.sort((a, b) => a - b);
-    return sorted.filter((item, index) => sorted.indexOf(item) === index);
+    return array.sort((a, b) => a - b).filter((item, index) => array.indexOf(item) === index);
+  }
+
+  // Build a tree out of the array
+  buildTree(array, start, end) {
+    // Build tree will take in an array and build the tree with it
+    if (start > end) {
+      return null;
+    }
+    // Find the middle of the array (root)
+    let mid = Math.floor((start + end) / 2);
+    // console.log(mid);
+    // Create a root element by passing array at mid index into the Node class
+    let root = new Node(array[mid]);
+    // Recursion:
+    // --Set the left node to buildTree, and pass in the array with the start being start, and end being mid-1
+    root.left = this.buildTree(array, start, mid - 1);
+    // --Set the right node to buildTree, and pass in the array with the start being mid+1, and the end being end
+    root.right = this.buildTree(array, mid + 1, end);
+    // Return the root;
+    return root;
   }
 }
