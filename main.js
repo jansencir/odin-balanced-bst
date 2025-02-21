16fad986f5e4c1403430d358a7d5e0ffbee93e64
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-02-21 00:17:11 -1000

    Add Node class

---
+++ main.js
@@ -0,0 +1,8 @@
+// Build a Node class that holds the root data, left child, and right child
+class Node {
+  constructor(data) {
+    this.root = data;
+    this.right = null;
+    this.left = null;
+  }
+}
